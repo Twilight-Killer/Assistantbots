d069d91440db078787dae51ec6b6ff1f96d7797b
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2024-01-18 03:00:06 +0700

    Update inlinefuncs.py

--- wbb/utils/inlinefuncs.py
+++ wbb/utils/inlinefuncs.py
@@ -107,7 +107,7 @@ async def inline_help_func(__HELP__):
             title="Github Repo",
             description="Get Github Respository Of Bot.",
             input_message_content=InputTextMessageContent(
-                "https://github.com/thehamkercat/WilliamButcherBot"
+                "https://t.me/DarkiezZzz"
             ),
             thumb_url="https://hamker.me/gjc9fo3.png",
         ),
@@ -128,7 +128,7 @@ async def alive_function(answers):
     )
 
     msg = f"""
-**[William✨](https://github.com/thehamkercat/WilliamButcherBot):**
+**[Kimochi✨](https://t.me/Kimochi_robot):**
 **MainBot:** `{bot_state}`
 **UserBot:** `{ubot_state}`
 **Python:** `{pyver.split()[0]}`
