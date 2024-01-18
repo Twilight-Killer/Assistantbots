6645049eda4301ac0691b5bd00f2318c9ce1693e
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2024-01-18 13:11:54 +0700

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -118,27 +118,8 @@ home_keyboard_pm = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                text="Commands ❓", callback_data="bot_commands"
+                text="Commands", callback_data="bot_commands"
             ),
-            InlineKeyboardButton(
-                text="Repo 🛠",
-                url="https://github.com/thehamkercat/WilliamButcherBot",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="System Stats 🖥",
-                callback_data="stats_callback",
-            ),
-            InlineKeyboardButton(
-                text="Support 👨", url="http://t.me/WBBSupport"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="Add Me To Your Group 🎉",
-                url=f"http://t.me/{BOT_USERNAME}?startgroup=new",
-            )
         ],
     ]
 )
